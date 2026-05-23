d80dc3b127e287665e442cfef64b1cf5f658ecc0
zhozhyr <zhozhyr@gmail.com>
2026-05-23 19:35:59 +0300

    fix ci

--- scripts/cassandra-init.sh
+++ scripts/cassandra-init.sh
@@ -2,5 +2,5 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-envsubst < "${SCRIPT_DIR}/cassandra-init.cql" | cqlsh "${CASSANDRA_HOSTS}" "${CASSANDRA_PORT}"
+sed "s/\${CASSANDRA_KEYSPACE}/${CASSANDRA_KEYSPACE}/g" "${SCRIPT_DIR}/cassandra-init.cql" | cqlsh "${CASSANDRA_HOSTS}" "${CASSANDRA_PORT}"
 echo "Cassandra schema init done."
